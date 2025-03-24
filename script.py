2e8c3a7bd25266a14cd1f3e4f213b1128ffdddd7
Hubmann Tomas Alejandro <tomas.hubmann@mi.unc.edu.ar>
2025-03-24 18:05:01 -0300

    loading data in the json

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+import json
 
 GCC_FLAGS_TO_TEST = {
     0: "-O0",
@@ -41,12 +42,10 @@ def parse_perf_output(perf_output: str) -> dict:
     Parses the output from `perf stat` and extracts performance metrics into a dictionary.
     """
     perf_data = {}
-    print("\n\n\n\n")
 
     # Define regex patterns for extracting metrics
-    print(perf_output)
     patterns = {
-        "cells_burned_per_micro_sec": r"cells_burned_per_micro_sec:\s*([\d\.]+)",
+        "cells_procesed_per_micro_sec": r"cells_burned_per_micro_sec:\s*([\d\.]+)",
         "cycles": r"([\d,]+) +cycles",
         "insn_per_cycle": r"([\d,.]+) +insn per cycle",
         "branches": r"([\d,]+) +branches",
@@ -120,12 +119,12 @@ def run_with_different_amount_of_simulations(data:str, amount_of_tries:int = 1) 
     code_file = "./graphics/burned_probabilities_data"
     stats = []
     flags= "-O3"
-    amount_of_sims = [128, 256, 512, 1024, 2048, 4096, 8192, 16384]
+    amount_of_sims = [128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
     for sim_amount in amount_of_sims:
         subprocess.run("make clean", shell=True)
         subprocess.run(f"make EXTRACXXFLAGS='{flags}'  DEFINES=-DSIMULATIONS={sim_amount}", shell=True)
-        perf_stats = {}
-        for n in range(amount_of_tries):
+        for k in range(36):
+            perf_stats = {}
             result = subprocess.run(f"perf stat ./{code_file} {data}", shell=True, stderr=subprocess.PIPE, text=True)
             last_value =  parse_perf_output(result.stderr)
             if not perf_stats:
@@ -135,11 +134,13 @@ def run_with_different_amount_of_simulations(data:str, amount_of_tries:int = 1) 
                     perf_stats[key] += value  # Add the current value to the sum
                 else:
                     perf_stats[key] = value  # If the key doesn't exist, just set it
-        # Average the statistics by dividing the accumulated sum by the number of tries
-        print(perf_stats)
-        perf_stats.update({"flag": flags, "data_name": data[0], "size_of_matrix": data[1], "simulations": sim_amount})
-        stats.append(perf_stats.copy())
+            # Average the statistics by dividing the accumulated sum by the number of tries
+            print(perf_stats)
+            perf_stats.update({"data_name": data, "simulations": sim_amount})
+
+            stats.append(perf_stats.copy())
     return stats
+
 
 code_file = "./graphics/burned_probabilities_data"
 data_file = "./data/2005_26"
@@ -155,6 +156,7 @@ for i in range(len(stats)):
     print(f"Time elapsed: {time_elapsed} seconds")
     print("****************************************************************************************\n")
 """
+"""
 stats = run_gcc_with_all_flags(code_file, data_file,1)
 print(stats)
 for i in range(len(stats)):
@@ -165,8 +167,20 @@ for i in range(len(stats)):
     print(f"Flag: {flag}")
     print(f"Time elapsed: {time_elapsed} seconds")
     print("****************************************************************************************\n")
-"""
 
+"""
 stats = run_with_different_amount_of_simulations(data_file, 1)
+with open("data.json", "w") as json_file:
+    json.dump(stats, json_file, indent=4)
 """
+for i in range(len(stats)):
+    time_elapsed = stats[i]['time_elapsed']
+    cells_per_micro_sc = stats[i]['cells_procesed_per_micro_sec']
+    flag = stats[i]['simulations']
+    print("\n****************************************************************************************")
+    print(f"Time elapsed: {time_elapsed} seconds with {flag} simulations")
+    print(f"Cells processed per micro second: {cells_per_micro_sc}")
+    print("****************************************************************************************\n")
 
+
+"""
