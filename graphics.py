534c9c473ead8daebbe98681fa564dec5448f973
yesica-escobares <yesica.escobares@mi.unc.edu.ar>
2025-03-25 21:36:55 -0300

    Graficos+videos

--- graphics.py
+++ graphics.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+# pip install moviepy      (si no lo tienes instalado)
+from moviepy import ImageClip, TextClip, CompositeVideoClip, concatenate_videoclips
 
 
 metrics = ["cells_procesed_per_micro_sec",'instructions','branches','time_elapsed',"cycles","insn_per_cycle","branch_misses", "user_time","sys_time",]
@@ -46,4 +48,38 @@ def draw_generic_grafic(df:pd.DataFrame, x_label:str):
 file_to_use = "csv_info/data_all_flags_2005.csv"
 
 df = pd.read_csv(file_to_use)
-draw_grafic_for_flags(df)
+#draw_grafic_for_flags(df)
+
+def video(labels, imagenes, video_name):
+    clips = []
+
+    for img, titulo in zip(imagenes, titulos):
+        # Crea un clip a partir de la imagen. La duración se puede ajustar (ej. 3 segundos)
+        imagen_clip = ImageClip(img).set_duration(3)
+        
+        # Crea un clip de texto para el título. Ajusta fuente, tamaño, color y posición según necesites.
+        texto_clip = TextClip(titulo, fontsize=50, color='white', font='Amiri-Bold')
+        texto_clip = texto_clip.set_duration(3).set_position(("center", "bottom"))
+        
+        # Superpone el texto sobre la imagen
+        clip_composite = CompositeVideoClip([imagen_clip, texto_clip])
+        clips.append(clip_composite)
+
+    # Une todos los clips en un solo video
+    video_final = concatenate_videoclips(clips, method="compose")
+
+    # Guarda el video en un archivo
+    video_final.write_videofile(f"{video_name}.mp4", fps=24)
+
+
+
+# Lista de nombres de archivo de las imágenes
+imagenes = ["/home/yesi/Documentos/FAMAF/ComputacionParalela/fire_spread_lab2025/1999_27j_S_burned_probabilities_O0.png",
+            "/home/yesi/Documentos/FAMAF/ComputacionParalela/fire_spread_lab2025/1999_27j_S_burned_probabilities_O1.png",
+            "/home/yesi/Documentos/FAMAF/ComputacionParalela/fire_spread_lab2025/1999_27j_S_burned_probabilities_O2.png" 
+            "/home/yesi/Documentos/FAMAF/ComputacionParalela/fire_spread_lab2025/1999_27j_S_burned_probabilities_O3.png"]
+
+# Lista de títulos para cada imagen (uno por ejecución, por ejemplo)
+titulos = ["-O0", "-O1", "-O2","-O3"]
+
+video(titulos, imagenes, "video_prueba")
--- script.py
+++ script.py
@@ -211,6 +211,6 @@ df = pd.DataFrame(stats)
 # Convertir la columna de flags a string para mejor visualización en los gráficos
 df["flag"] = df["flag"].astype(str)
 
-df.to_csv(f"csv_info/run_all_cases.csv", index=False)
+df.to_csv(f"csv_info/prueba.csv", index=False)
 
 
